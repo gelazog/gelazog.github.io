5a4ec1e711892c70940fe24f474c02d4e30090c7
gelazog <140112774+gelazog@users.noreply.github.com>
2025-03-22 01:48:23 -0700

    Update loadcomponents.js

--- js/loadcomponents.js
+++ js/loadcomponents.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const basePath = window.location.pathname.includes("/pages/") ? "../partials/" : "./partials/";
-    
+document.addEventListener("DOMContentLoaded", function () {
+    // Detecta si la URL contiene /pages/ y ajusta la ruta
+    const basePath = window.location.pathname.includes("pages/") ? "../partials/" : "./partials/";
+
     loadComponent("header", basePath + "header.html");
     loadComponent("footer", basePath + "footer.html");
 });
@@ -14,7 +15,12 @@ function loadComponent(id, url) {
             return response.text();
         })
         .then(data => {
-            document.getElementById(id).innerHTML = data;
+            const element = document.getElementById(id);
+            if (element) {
+                element.innerHTML = data;
+            } else {
+                console.error(`Elemento con ID ${id} no encontrado.`);
+            }
         })
         .catch(error => console.error(error));
 }
